f29acf74ef20eda4ae5fd1313f2d62e661da5ce1
Kevinomini <kevinomini@gmail.com>
2024-01-17 12:39:13 +0100

    update base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://habaripay.github.io",
+  url: "https://docs.squadco.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/squad-docs/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
