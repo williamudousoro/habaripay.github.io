5968085abcbc14c170afbc2e24603e74b67e1679
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2024-01-29 14:12:47 +0100

    Added images

--- src/components/WooCommerce/index.jsx
+++ src/components/WooCommerce/index.jsx
@@ -1,22 +1,34 @@
 import React from "react";
+import { Cloudinary } from "@cloudinary/url-gen";
+import { AdvancedImage } from "@cloudinary/react";
 
 const WooCommerce = "img/woo-commerce.png";
 const OrderImg = "img/place-order.png";
 const ModalImg = "img/wooModal.png";
 
 const WooImgComponent = () => {
+  const cld = new Cloudinary({ cloud: { cloudName: "delflsgq4" } });
   return (
     <div className="grid place-items-center w-full text-center text-gray-500 dark:text-gray-400 text-sm">
       <div>
-        <img src={WooCommerce} alt="Ussd Modal" className="" />
+        <AdvancedImage
+          className="w-[80%]"
+          cldImg={cld.image("squad-docs/woo-commerce")}
+        />
         <p>1/3 - Squad Settings</p>
       </div>
       <div>
-        <img src={OrderImg} alt="Ussd Modal" className="" />
+        <AdvancedImage
+          className="w-full"
+          cldImg={cld.image("squad-docs/place-order")}
+        />
         <p>2/3 - Sample Order Page</p>
       </div>
       <div>
-        <img src={ModalImg} alt="Ussd Modal" className="" />
+        <AdvancedImage
+          className="w-full"
+          cldImg={cld.image("squad-docs/wooModal")}
+        />
         <p>3/3 - Squad Payment Modal</p>
       </div>
     </div>
