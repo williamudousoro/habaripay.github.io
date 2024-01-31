475bb803d5291abee5b6be776ebeb1bdb14391d1
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2024-01-31 16:06:25 +0100

    Clean up (#1)
    * Cleaned up code
    
    * Made changes

--- src/components/TestCard/index.jsx
+++ src/components/TestCard/index.jsx
@@ -4,32 +4,7 @@ import { AdvancedImage } from "@cloudinary/react";
 import CopyToClipboard from "../CopyToClipboard/CopyToClipboard";
 
 function TestCard() {
-  const currentDate = new Date();
-  const currentYear = currentDate.getFullYear();
-
-  const [isCopied, setIsCopied] = useState(false);
-  const [isHovered, setIsHovered] = useState(false);
-
   const cld = new Cloudinary({ cloud: { cloudName: "delflsgq4" } });
-
-  const backgroundImageStyle = {
-    backgroundImage: 'url("/static/img/frontCard.svg")',
-    backgroundSize: "cover",
-    width: "300px",
-    height: "200px",
-  };
-
-  const copyToClipboard = (text) => {
-    navigator.clipboard.writeText(text).then(
-      () => {
-        setIsCopied(true);
-        setTimeout(() => setIsCopied(false), 2000); // Reset copied status after 2 seconds
-      },
-      (err) => {
-        console.error("Failed to copy: ", err);
-      }
-    );
-  };
 
   return (
     <div className=" w-full grid md:grid-cols-2 grid-cols-1 gap-5 hover:cursor-pointer">
