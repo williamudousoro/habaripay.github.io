1ac2ee5907ead74d859bbb9965f7240eed3ee174
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2024-01-31 12:27:21 +0100

    Card number copy functionality

---
+++ src/components/CopyToClipboard/CopyToClipboard.tsx
@@ -0,0 +1,46 @@
+import React, { ReactNode, useRef } from "react";
+
+interface Props {
+  children: ReactNode;
+  copyData: string;
+}
+
+export default function CopyToClipboard(props: Props) {
+  const { children, copyData } = props;
+
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const container = useRef<HTMLDivElement>(null);
+  const [text, setText] = React.useState("Copy card number");
+
+  const onCopy = (e: Event) => {
+    e.preventDefault();
+    e.stopPropagation();
+    navigator.clipboard.writeText(copyData);
+    setText("copied");
+  };
+
+  return (
+    <div
+      ref={container}
+      onMouseEnter={({ clientX }) => {
+        if (!buttonRef.current || !container.current) return;
+        const { left } = container.current.getBoundingClientRect();
+
+        buttonRef.current.style.left = clientX - left + "px";
+      }}
+      onMouseLeave={() => setText("Copy card number")}
+      className={`group relative inline-block`}
+    >
+      {children}
+      <button
+        onClick={onCopy as any}
+        ref={buttonRef}
+        className={`invisible group-hover:visible opacity-0 group-hover:opacity-100 transition 
+          bg-white text-normal/80 py-1 px-2.5 rounded absolute top-[50%] z-10 w-fit whitespace-nowrap text-sm 
+          border border-gray-700 custom-shadow-md`}
+      >
+        {text}
+      </button>
+    </div>
+  );
+}
--- src/components/TestCard/index.jsx
+++ src/components/TestCard/index.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Cloudinary } from "@cloudinary/url-gen";
 import { AdvancedImage } from "@cloudinary/react";
+import CopyToClipboard from "../CopyToClipboard/CopyToClipboard";
 
 function TestCard() {
   const currentDate = new Date();
   const currentYear = currentDate.getFullYear();
+
+  const [isCopied, setIsCopied] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
 
   const cld = new Cloudinary({ cloud: { cloudName: "delflsgq4" } });
 
@@ -15,12 +19,27 @@ function TestCard() {
     height: "200px",
   };
 
+  const copyToClipboard = (text) => {
+    navigator.clipboard.writeText(text).then(
+      () => {
+        setIsCopied(true);
+        setTimeout(() => setIsCopied(false), 2000); // Reset copied status after 2 seconds
+      },
+      (err) => {
+        console.error("Failed to copy: ", err);
+      }
+    );
+  };
+
   return (
-    <div className=" w-full grid md:grid-cols-2 grid-cols-1 gap-5">
-      <AdvancedImage
-        className="w-full"
-        cldImg={cld.image("squad-docs/frontCard")}
-      />
+    <div className=" w-full grid md:grid-cols-2 grid-cols-1 gap-5 hover:cursor-pointer">
+      <CopyToClipboard copyData={"4242424242424242"}>
+        <AdvancedImage
+          className="w-full "
+          cldImg={cld.image("squad-docs/frontCard")}
+        />
+      </CopyToClipboard>
+
       <AdvancedImage
         className="w-full"
         cldImg={cld.image("squad-docs/backCard")}
